cb30ecb7a7f9743eed8ca8603eb345d24fbbb68f
aryanmayor <57047146+aryanmayor@users.noreply.github.com>
2019-11-03 23:52:11 -0500

    Added my email

--- mailer.php
+++ mailer.php
@@ -22,7 +22,7 @@
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "youremail@gmail.com";
+        $recipient = "aryan.mayor@gmail.com";
 
         // Set the email subject.
         $subject = "New Message from ".$personName.' through your website KRIN';
